feaa2d4b8681f0e20f95e9c262b4b30ba2f31e4d
Attila Szegedi <attila.szegedi@datadoghq.com>
2023-08-18 14:42:54 +0200

    First prototype of a timeline profiler

--- packages/dd-trace/src/profiling/profilers/wall.js
+++ packages/dd-trace/src/profiling/profilers/wall.js
@@ -7,6 +7,7 @@ const { HTTP_METHOD, HTTP_ROUTE, RESOURCE_NAME, SPAN_TYPE } = require('../../../
 const { WEB } = require('../../../../../ext/types')
 const runtimeMetrics = require('../../runtime_metrics')
 const telemetryMetrics = require('../../telemetry/metrics')
+const { END_TIMESTAMP, ROOT_SPAN_ID, SPAN_ID } = require('./shared')
 
 const beforeCh = dc.channel('dd-trace:storage:before')
 const enterCh = dc.channel('dd-trace:storage:enter')
@@ -23,13 +24,13 @@ function getStartedSpans (context) {
   return context._trace.started
 }
 
-function generateLabels ({ spanId, rootSpanId, webTags, endpoint }) {
+function generateLabels ({ context: { spanId, rootSpanId, webTags, endpoint }, timestamp }) {
   const labels = {}
   if (spanId) {
-    labels['span id'] = spanId
+    labels[SPAN_ID] = spanId
   }
   if (rootSpanId) {
-    labels['local root span id'] = rootSpanId
+    labels[ROOT_SPAN_ID] = rootSpanId
   }
   if (webTags && Object.keys(webTags).length !== 0) {
     labels['trace endpoint'] = endpointNameFromTags(webTags)
@@ -37,7 +38,7 @@ function generateLabels ({ spanId, rootSpanId, webTags, endpoint }) {
     // fallback to endpoint computed when sample was taken
     labels['trace endpoint'] = endpoint
   }
-
+  labels[END_TIMESTAMP] = timestamp * 1000n
   return labels
 }
 
